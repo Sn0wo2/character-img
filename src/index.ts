3333a7e6050339910a63a4fa98c1ca5b4b1d1c95
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-20 14:00:45 +0800

    refactor: compose

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import { getImageHandler } from './handler/image';
 import { corsMiddleware } from './middleware/cors';
 
-function compose(middleware: Array<(req: Request, next: (req: Request) => Promise<Response>) => Promise<Response>>) {
-	return function(request: Request): Promise<Response> {
+type Middleware = (
+	request: Request,
+	next: (request: Request) => Promise<Response>
+) => Promise<Response>;
+
+function compose(...middlewares: Middleware[]) {
+	return async function(request: Request): Promise<Response> {
 		let index = -1;
 
-		async function dispatch(i: number, req: Request): Promise<Response> {
-			if (i <= index) throw new Error('next() called multiple times');
+		const dispatch = async (i: number, req: Request): Promise<Response> => {
+			if (i <= index) {
+				throw new Error('next() called multiple times');
+			}
 			index = i;
-			let fn = middleware[i];
-			if (i === middleware.length) fn = getImageHandler;
-			if (!fn) throw new Error('No middleware or handler');
-			return fn(req, (r) => dispatch(i + 1, r));
-		}
+
+			const middleware = middlewares[i];
+
+			if (middleware) {
+				return middleware(req, (nextRequest) => dispatch(i + 1, nextRequest ?? req));
+			}
+
+			return new Response('Not Found', { status: 404 });
+		};
 
 		return dispatch(0, request);
 	};
@@ -20,9 +31,9 @@ function compose(middleware: Array<(req: Request, next: (req: Request) => Promis
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const handler = compose([
-			corsMiddleware
-		]);
-		return handler(request);
+		return compose(
+			corsMiddleware,
+			getImageHandler
+		)(request);
 	}
 } satisfies ExportedHandler<Env>;
